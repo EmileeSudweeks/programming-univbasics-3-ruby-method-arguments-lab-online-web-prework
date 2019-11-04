4c362f59f12c8f6ee046e07da7e895a43a580927
Emilee Sudweeks <emilee.sudweeks@wework.com>
2019-11-04 19:09:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,8 @@
-
+describe "#introduction_with_language_optional" do
+  it "takes in two arguments, a name and a language, and language defaults to Ruby" do
+    expect{introduction_with_language_optional("Edwin")}.to output("Hi, my name is Edwin and I am learning to program in Ruby.\n").to_stdout
+  end
+end
 
 
 
